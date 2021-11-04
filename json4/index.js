f0e0f20c5d2e3158ecac8c07c53afa47662e00b8
AleksanderKlobuk <64355151+AleksanderKlobuk@users.noreply.github.com>
2021-11-04 09:44:55 +0100

    Add files via upload

---
+++ json4/index.js
@@ -0,0 +1,83 @@
+async function loadData() {
+  const response = await fetch("https://raw.githubusercontent.com/Solnick/users/master/db.json");
+  return response.json();
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const jsondata_var = await loadData();
+    const divContainer = document.getElementById('content');
+      var table_data = "";
+
+      
+      
+      jsondata_var.users.forEach((itemData) => {
+        table_data += "<tr>";
+        table_data += "<td>" + itemData.uris.company.substr(11) +  "</td>";
+        table_data += "<td>" + itemData.email + "</td>";
+      })
+
+      var table_data2 = "";
+
+      /*users_var.companies.forEach((itemData) => {        
+        table_data += "<tr>";
+        table_data += "<td>" + itemData.name +  "</td>"+"</tr>";
+      })
+      users_var.users.forEach((itemData2) => {        
+        table_data2 += "<tr>";
+        table_data2 += "<td>" + itemData2.name +  "</td>"+"</tr>";
+      })*/
+
+
+      /*const a =users_var.companies.name 
+      const b =users_var.users.name 
+      a.forEach((itemData1) => {
+        table_data += "<tr>"+ "<td>" + itemData1+ "</td>" +"</tr>";
+        table_data += "<tr>"+ "<td>" + itemData1+ "</td>" +"</tr>";
+  
+      /*b.forEach((itemData2) => {
+        if (itemData1.name == itemData2.name) {
+        table_data += "<tr>"+ "<td>" + itemData2.name+ "</td>" +"</tr>";
+        table_data += "<tr>"+ "<td>" + itemData2.name+ "</td>" +"</tr>";
+      }})*/
+
+      
+
+
+
+
+      document.getElementById('data').innerHTML = table_data;
+      document.getElementById('data2').innerHTML = table_data2;
+  } catch (e) {
+    console.log('ERROR');
+    console.log(e);
+  }
+  });
+
+ /*fetch("https://raw.githubusercontent.com/Solnick/users/master/db.json").then(
+  res => {
+    res.json().then(
+      dane => {
+        console.log(dane.users);
+        console.log(dane.companies);
+          if (dane.users.length > 0) {
+
+          var temp = "";
+          dane.users.forEach((itemData) => {
+            temp += "<tr>"; 
+            temp += "<td>" + itemData.uris.company + "</td>";
+            temp += "<td>" + itemData.name + "</td>";
+            temp += "<td>" + itemData.email + "</td>";
+          });
+          dane.companies.forEach((itemData2) => {
+            temp += "<td>" + itemData2.name + "</td></tr>";
+          });
+
+          document.getElementById('data').innerHTML = temp;
+
+        }
+      }
+    ) 
+  }
+)
+*/
